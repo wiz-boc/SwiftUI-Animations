722ded0dd663c1f43c92cb1469041f45425d09c9
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-09-23 06:56:12 -0500

    HueRotation app 2

---
+++ HueRotation2/HueRotation2/HueRotation2App.swift
@@ -0,0 +1,17 @@
+//
+//  HueRotation2App.swift
+//  HueRotation2
+//
+//  Created by wizz on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct HueRotation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
