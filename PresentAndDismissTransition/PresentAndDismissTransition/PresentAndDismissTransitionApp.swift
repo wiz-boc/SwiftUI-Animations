f84e137254b82a93f1ce6074f5c41f3f1c586a0d
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-09-26 19:05:12 -0500

    PresentAndDismissTransition

---
+++ PresentAndDismissTransition/PresentAndDismissTransition/PresentAndDismissTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentAndDismissTransitionApp.swift
+//  PresentAndDismissTransition
+//
+//  Created by wizz on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct PresentAndDismissTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
